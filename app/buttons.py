34bb99f54ca3cfadc78b03ccbf0ff712d7a23993
kriokinetik <sadiev2005@mail.ru>
2023-10-10 00:02:58 +0300

    Rename buttons to clarify their's purpose

--- app/buttons.py
+++ app/buttons.py
@@ -3,12 +3,12 @@ from aiogram.types import InlineKeyboardButton
 bei2202 = InlineKeyboardButton(text='БЭИ2202', callback_data='group_БЭИ2202')
 pozorniki = InlineKeyboardButton(text='позорники🥰🥰🥰', callback_data='group_позорники🥰🥰🥰')
 
-add_shame = InlineKeyboardButton(text='+позор', callback_data='add_shame')
-check_history = InlineKeyboardButton(text='история', callback_data='check_history')
+add_shame = InlineKeyboardButton(text='Добавить позор', callback_data='add_shame')
+check_history = InlineKeyboardButton(text='Просмотр истории', callback_data='check_history')
 
 back = InlineKeyboardButton(text='⬅️', callback_data='back')
 forward = InlineKeyboardButton(text='➡️', callback_data='forward')
 
-delete_message = InlineKeyboardButton(text='delete', callback_data='delete_message')
+delete_message = InlineKeyboardButton(text='Удалить сообщение', callback_data='delete_message')
 
-home = InlineKeyboardButton(text='🏠', callback_data='home')
+home = InlineKeyboardButton(text='На главную', callback_data='home')
