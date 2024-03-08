7ce25eedda486d12b8e45b75e4fa2c30b16ee35d
kriokinetik <sadiev2005@mail.ru>
2024-03-09 01:57:04 +0300

    Rewrite record storage mechanism, now records are unique for each chat room Make file I/O asynchronous Code refactoring Set HTML as bot's default parsing mode Add error handling for admin commands Change data file name

--- app/handlers.py
+++ app/handlers.py
@@ -7,133 +7,160 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
 from app import keyboards, json_file, filters
-from app.states import ShameData
+from app.states import RecordData
+from config import JSON_FILE_NAME
 
 router = Router()
 
 
-@router.message(Command('start', 'help'))
-async def bot_call_handler(message: Message):
-    await message.reply(text='/add@ShameCounter_Bot — добавить запись\n'
-                             '/history@ShameCounter_Bot — просмотреть историю')
-
-
-@router.message(Command('add'))
-async def add_record_handler(message: Message, state: FSMContext):
-    data = await state.get_data()
-    if data:
+async def check_and_reply_empty_group(message: Message, state: FSMContext) -> bool:
+    if not await json_file.check_records_existance(JSON_FILE_NAME, message.chat.id):
+        await message.reply('Группы и записи отсутствуют.')
         await state.clear()
-    await state.update_data(start_message=message)
-    if json_file.json_load('shame.json') == {}:
-        await message.reply(text='Группы и записи отсутствуют.')
-        await state.clear()
-        return
-    bot_message = await message.reply(text='Выберите группу.',
-                                      reply_markup=keyboards.get_group_selection_keyboard('shame.json'))
-    await state.update_data(message_access=bot_message.message_id, action='add')
+        return True
+    return False
 
 
-@router.message(Command('history'))
-async def show_history_handler(message: Message, state: FSMContext, reply_flag: bool = True):
-    data = await state.get_data()
-    if data:
-        await state.clear()
-    await state.update_data(start_message=message)
-    if json_file.json_load('shame.json') == {}:
-        await message.reply(text='Группы и записи отсутствуют.')
-        await state.clear()
-        return
-    if reply_flag:
-        bot_message = await message.reply(text='Выберите группу.',
-                                          reply_markup=keyboards.get_group_selection_keyboard('shame.json'))
-    else:
-        bot_message = await message.answer(text='Выберите группу.',
-                                           reply_to_message_id=message.reply_to_message.message_id,
-                                           reply_markup=keyboards.get_group_selection_keyboard('shame.json'))
-    await state.update_data(message_access=bot_message.message_id, action='history')
-
-
-async def request_a_record(callback: CallbackQuery, state: FSMContext):
+async def request_record(callback: CallbackQuery, state: FSMContext) -> None:
     data = await state.get_data()
     bot_message = await callback.bot.send_message(chat_id=callback.message.chat.id,
                                                   text=f'Группа "{data["group_name"]}".\n\n'
                                                        f'Опишите ситуацию.',
-                                                  reply_to_message_id=data['start_message'].message_id,
+                                                  reply_to_message_id=data["initial_message"].message_id,
                                                   reply_markup=ForceReply(selective=True)
                                                   )
-    await state.update_data(message_access=bot_message.message_id)
+    await state.update_data(interaction_right=bot_message.message_id)
     await callback.message.delete()
-    await state.set_state(ShameData.description)
+    await state.set_state(RecordData.record_content)
 
 
-async def show_history(callback: CallbackQuery, state: FSMContext):
-    data = await state.get_data()
-    if os.path.exists('shame.json'):
-        number_of_records = json_file.get_the_record_number('shame.json', data['group_name'])
-        if number_of_records != 0:
-            await state.update_data(shame_id=0)
-            data = await state.get_data()
-            shame_data = json_file.get_the_shame_data('shame.json', data['group_name'], data['shame_id'])
-            await callback.message.edit_text(text=f'Группа "{data["group_name"]}".\n\n'
-                                                  f'{shame_data}',
-                                             reply_markup=keyboards.check_history)
-        else:
-            await callback.message.edit_text(text=f'Группа "{data["group_name"]}".\n\n'
-                                                  f'Записи отсутствуют.',
+async def edit_history_message(callback: CallbackQuery, group_name: str, record_id: int, record_count: int) -> None:
+    chat_id = callback.message.chat.id
+    record_data = await json_file.get_record_data(JSON_FILE_NAME, chat_id, group_name, record_id)
+    record_datetime = record_data["date"]
+    record_content = record_data["description"]
+    await callback.message.edit_text(
+        text=f'Группа "{group_name}". Запись {record_id + 1}/{record_count}\n\n'
+             f'{record_datetime}\n'
+             f'{record_content}',
+        reply_markup=keyboards.check_history
+    )
+
+
+async def show_history(callback: CallbackQuery, state: FSMContext) -> None:
+    group_name = (await state.get_data())["group_name"]
+    chat_id = callback.message.chat.id
+    if os.path.exists(JSON_FILE_NAME):
+        record_count = await json_file.get_record_count(JSON_FILE_NAME, chat_id, group_name)
+        if record_count == 0:
+            await callback.message.edit_text(text=f'Группа "{group_name}".\n\nЗаписи отсутствуют.',
                                              reply_markup=keyboards.go_back_keyboard)
+            return
+
+        await state.update_data(record_id=0)
+        await edit_history_message(callback, group_name, 0, record_count)
 
 
-@router.callback_query(F.data.startswith('group'), filters.CallbackAccess())
+# Обработчики событий
+@router.message(Command('start'))
+async def bot_call_handler(message: Message) -> None:
+    await message.reply(
+        'Привет! Я бот для управления записями по группам. С моей помощью ты можешь создавать группы и '
+        'добавлять записи к каждой из них. Позже ты сможешь легко просматривать записи по каждой группе.\n\n'
+        'Чтобы начать, используй команду /create для создания новой группы или /add_record для '
+        'добавления записи к уже существующей.\n\n'
+        'Для получения справки по командам: /help'
+    )
+    await json_file.start_json_session(JSON_FILE_NAME, message.chat.id)
+
+
+@router.message(Command('help'))
+async def bot_call_handler(message: Message) -> None:
+    await message.reply('Справка по командам:\n\n'
+                        '/add — добавить запись.\n'
+                        '/create <code>название</code> — создать указанную группу.\n'
+                        '/history — просмотреть историю.\n'
+                        '/edit — отредактировать <i>открытую ранее</i> запись.\n'
+                        '/delete_record — удалить <i>открытую ранее</i> запись.\n'
+                        '/delete_group <code>название</code> — удалить указанную группу.\n'
+                        '/delete_message<i>*</i> — удалить сообщение.\n\n'
+                        '<i>*</i> — команды нужно вызывать ответом на сообщение.\n')
+
+
+@router.message(Command('add'))
+async def add_record_handler(message: Message, state: FSMContext):
+    await state.clear()
+    await state.update_data(initial_message=message)
+    if await check_and_reply_empty_group(message, state):
+        return
+    bot_message = await message.reply(text='Выберите группу.',
+                                      reply_markup=await keyboards.get_group_selection_keyboard(message,
+                                                                                                JSON_FILE_NAME))
+    await state.update_data(interaction_right=bot_message.message_id,
+                            action='add')
+
+
+@router.message(Command('history'))
+async def show_history_handler(message: Message, state: FSMContext, reply_flag: bool = True):
+    await state.clear()
+    await state.update_data(initial_message=message)
+    if await check_and_reply_empty_group(message, state):
+        return
+    if reply_flag:
+        bot_message = await message.reply(text='Выберите группу.',
+                                          reply_markup=await keyboards.get_group_selection_keyboard(message,
+                                                                                                    JSON_FILE_NAME))
+    else:
+        bot_message = await message.answer(text='Выберите группу.',
+                                           reply_to_message_id=message.reply_to_message.message_id,
+                                           reply_markup=await keyboards.get_group_selection_keyboard(message,
+                                                                                                     JSON_FILE_NAME))
+    await state.update_data(interaction_right=bot_message.message_id, action='history')
+
+
+@router.callback_query(F.data.startswith('group'), filters.CallbackAccessFilter())
 async def action_handler(callback: CallbackQuery, state: FSMContext):
-    data = await state.update_data(group_name=callback.data[6:])
-    if data['action'] == 'add':
-        await request_a_record(callback, state)
-    elif data['action'] == 'history':
+    group_name = callback.data[6:]
+    data = await state.update_data(group_name=group_name)
+    if data["action"] == 'add':
+        await request_record(callback, state)
+    elif data["action"] == 'history':
         await show_history(callback, state)
 
 
-@router.callback_query(F.data == 'next_record', filters.CallbackAccess())
-async def next_record_handler(callback: CallbackQuery, state: FSMContext):
+@router.callback_query(F.data.in_({'next_record', 'prev_record'}), filters.CallbackAccessFilter())
+async def navigate_records_handler(callback: CallbackQuery, state: FSMContext):
     data = await state.get_data()
-    last_id = json_file.get_the_record_number('shame.json', data['group_name']) - 1
-    if data['shame_id'] == last_id:
-        next_id = 0
-        data = await state.update_data(shame_id=next_id)
+    chat_id = callback.message.chat.id
+    group_name = data["group_name"]
+    record_id = data.get("record_id", 0)
+    record_count = await json_file.get_record_count(JSON_FILE_NAME, chat_id, group_name)
+
+    if record_count == 1:
+        await callback.answer()
+        return
+
+    if callback.data == 'next_record':
+        record_id = (record_id + 1) % record_count
     else:
-        next_id = data['shame_id'] + 1
-        data = await state.update_data(shame_id=next_id)
-    shame_data = json_file.get_the_shame_data('shame.json', data['group_name'], data['shame_id'])
-    await callback.message.edit_text(text=f'Группа "{data["group_name"]}".\n\n'
-                                          f'{shame_data}',
-                                     reply_markup=keyboards.check_history)
+        record_id = (record_id - 1) % record_count
+
+    await state.update_data(record_id=record_id)
+    await edit_history_message(callback, group_name, record_id, record_count)
 
 
-@router.callback_query(F.data == 'prev_record', filters.CallbackAccess())
-async def prev_record_handler(callback: CallbackQuery, state: FSMContext):
-    data = await state.get_data()
-    if data['shame_id'] == 0:
-        next_id = json_file.get_the_record_number('shame.json', data['group_name']) - 1
-        data = await state.update_data(shame_id=next_id)
-    else:
-        next_id = data['shame_id'] - 1
-        data = await state.update_data(shame_id=next_id)
-    shame_data = json_file.get_the_shame_data('shame.json', data['group_name'], data['shame_id'])
-    await callback.message.edit_text(text=f'Группа "{data["group_name"]}".\n\n'
-                                          f'{shame_data}',
-                                     reply_markup=keyboards.check_history)
-
-
-@router.callback_query(F.data == 'go_back', filters.CallbackAccess())
+@router.callback_query(F.data == 'go_back', filters.CallbackAccessFilter())
 async def go_back_handler(callback: CallbackQuery, state: FSMContext):
     await show_history_handler(callback.message, state, reply_flag=False)
     await callback.message.delete()
 
 
-@router.message(ShameData.description, filters.MessageAccess())
+@router.message(RecordData.record_content, filters.MessageAccessFilter())
 async def description_handler(message: Message, state: FSMContext):
-    await state.update_data(description=message.text)
+    chat_id = message.chat.id
     delta = datetime.timedelta(hours=3, minutes=0)
-    data = await state.update_data(message_datetime=(message.date + delta).strftime('%d.%m.%y %H:%M'))
-    json_file.add_record_to_json('shame.json', data['group_name'], data['message_datetime'], data['description'])
-    await message.reply(text=f'В группу "{data["group_name"]}" добавлена запись.')
+    record_datetime = (message.date + delta).strftime('%d.%m.%y %H:%M')
+    group_name = (await state.get_data())["group_name"]
+    await json_file.add_record_to_json(JSON_FILE_NAME, chat_id, group_name, record_datetime, message.text)
+    await message.reply(f'В группу "{group_name}" добавлена запись.')
     await state.clear()
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,14 +1,20 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from app import buttons, json_file
 
 
-def get_group_selection_keyboard(file_name: str) -> InlineKeyboardMarkup:
-    groups = json_file.get_the_groups(file_name)
-    buttons = []
-    for name in groups:
-        buttons.append([InlineKeyboardButton(text=name, callback_data=f'group_{name}')])
-    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
-    return keyboard
+async def get_group_selection_keyboard(message: Message, file_name: str) -> InlineKeyboardMarkup:
+    chat_id = message.chat.id
+    groups = await json_file.get_groups(file_name, chat_id)
+    buttons = [
+        [InlineKeyboardButton(text=groups[i], callback_data=f'group_{groups[i]}'),
+         InlineKeyboardButton(text=groups[i + 1], callback_data=f'group_{groups[i + 1]}')]
+        for i in range(0, len(groups), 2)
+        if i + 1 < len(groups)
+    ]
+    # Если количество групп нечетное, добавляем последнюю кнопку
+    if len(groups) % 2 != 0:
+        buttons.append([InlineKeyboardButton(text=groups[-1], callback_data=f'group_{groups[-1]}')])
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
 
 
 check_history = InlineKeyboardMarkup(
