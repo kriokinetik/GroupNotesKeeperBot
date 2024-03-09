4ed09d7089c2ae73352db33403e7d2e6b1cd5d7c
kriokinetik <sadiev2005@mail.ru>
2024-03-09 17:22:24 +0300

    Make record deletion view similar to history view

--- app/keyboards.py
+++ app/keyboards.py
@@ -5,27 +5,43 @@ from app import buttons, json_file
 async def get_group_selection_keyboard(message: Message, file_name: str) -> InlineKeyboardMarkup:
     chat_id = message.chat.id
     groups = await json_file.get_groups(file_name, chat_id)
-    buttons = [
+
+    keyboard = [
         [InlineKeyboardButton(text=groups[i], callback_data=f'group_{groups[i]}'),
          InlineKeyboardButton(text=groups[i + 1], callback_data=f'group_{groups[i + 1]}')]
         for i in range(0, len(groups), 2)
         if i + 1 < len(groups)
     ]
+
     # Если количество групп нечетное, добавляем последнюю кнопку
     if len(groups) % 2 != 0:
-        buttons.append([InlineKeyboardButton(text=groups[-1], callback_data=f'group_{groups[-1]}')])
-    return InlineKeyboardMarkup(inline_keyboard=buttons)
+        keyboard.append([InlineKeyboardButton(text=groups[-1], callback_data=f'group_{groups[-1]}')])
+
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
 
 
-check_history = InlineKeyboardMarkup(
-    inline_keyboard=[
+async def get_navigate_record_keyboard(root: bool) -> InlineKeyboardMarkup:
+    keyboard = [
         [buttons.prev_record, buttons.next_record],
         [buttons.go_back]
     ]
-)
+
+    # Если вызвана команда /delete_record, добавляем кнопку "Удалить запись"
+    if root:
+        keyboard.append([buttons.delete_record])
+
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
+
 
 go_back_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
         [buttons.go_back]
+    ]
+)
+
+confirm_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [buttons.confirm_button],
+        [buttons.cancel_button]
     ]
 )
